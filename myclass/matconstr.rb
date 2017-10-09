3fb796cdb210fec87243602817c82b50c01fbd30
rubysqlgit <n54caire@excite.co.jp>
2017-10-09 17:53:27 +0900

    chain6

--- myclass/matconstr.rb
+++ myclass/matconstr.rb
@@ -1,23 +1,31 @@
 #! ruby -Ku
 
 class Ary_test
-  def initialize(mat = 1)
-    p @mat_to_str = mat  
-    story05
+  def initialize(input = 1)
+    @mat_to_str = input  
+    @mat = 1
+    @str = 1
     
-
-    story06
-    p @mat_to_str
-    
-    #story07
-    #story08
+    if input.is_a?(String)
+      @str = input
+      p "行列に変換"
+      story07
+      p @mat
+    else
+      @mat = input
+      p "文字列に変換"
+      story08
+      @str = @mat
+      p @str
+    end
+    #p @mat_to_str
   end
+  attr_accessor :mat, :str
   attr_accessor :mat_to_str
   
 ############行列から文字列####################  
   def story05
     karisome = []
-    
     @mat_to_str.map do |ary|
       ary.push(0) if ary.size == 0
     end  
@@ -25,7 +33,7 @@ class Ary_test
     @mat_to_str.map! do |ary|
       karisome.push(ary.join("."))
     end
-    p @mat_to_str = karisome.join("/")
+    @mat_to_str = karisome.join("/")
   end
   
   
@@ -38,11 +46,7 @@ class Ary_test
 
     karisome.map! do |ary|
       ary.map! do |i|
-        if i.to_i == 0
-          i = nil
-        else
-          i = i.to_i
-        end
+        i.to_i == 0 ? i = nil : i = i.to_i
       end
     end
     @mat_to_str = karisome
@@ -53,46 +57,49 @@ class Ary_test
   end
   
   def story07
-    b = [], [], [1], []
-    p b
-    
-    b.map do |ary|
-      if ary.size == 0
-        ary.push(0)
-      end
+    @mat = [] 
+    @str.split("/").map! do |ary|
+      @mat.push(ary.split("."))
     end
-    p b
-  end
-  
-  def story08
-    b = [0], [0], [1], [0]
-    p b
-    b.map do |ary|
-      ary.map! do |i|
-        i == 0 ? i = nil : i = i
-      end
-    end
-    
-    p b
-    
-    b.map do |i|
-      i.compact!
-    end
-    
-    p b
-  end
 
+    @mat.map! do |ary|
+      ary.map! do |i|
+        i.to_i == 0 ? i = nil : i = i.to_i
+      end
+    end
+    
+    @mat.map do |i|
+      i.compact!
+    end    
+  end
   
+  ##################
+  def story08
+    karisome = []
+    @mat.map do |ary|
+      ary.push(0) if ary.size == 0
+    end  
+      
+    @mat.map! do |ary|
+      karisome.push(ary.join("."))
+    end
+    @mat = karisome.join("/")
+  end
 end
+
+
 
 
 
 
 class BB
   def initialize
-    @tom = [[1,2,3],[],  [4,5,6],[]]
-    #@tom = [],[],[],[],[],[]
+    #@tom = [[1,2,3],[],  [4,5,6],[]]
+    @tom = [],[],[],[],[],[]
+    p @tom
+    #p @tom = "0/0/3.3.3/0/0/0"
     test = Ary_test.new(@tom)
+    #p test
     #p test.mat_to_str
   end
 end
