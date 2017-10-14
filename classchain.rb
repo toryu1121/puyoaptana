0031989956c33cbb363b29e512a5858667ad0cd6
rubysqlgit <n54caire@excite.co.jp>
2017-10-14 23:06:46 +0900

    chain15_2

--- classchain.rb
+++ classchain.rb
@@ -4,74 +4,67 @@ require './tdisp'
 class Chain
   def initialize(field)
     p "chain.new"
-    
     @field = field
-    Tdisp.new(@field)
-    
-    
     @fieldclone =[],[],[],[],[],[]
     @chainecount = 0
-    @field = Matrix.new(12, 6)
-    story01 #フィールドにランダム数字の代入
-    @field.tdisp
-    
-    
-    #多分メインの処理の部分でここがループしてるんでしょうね
+    Tdisp.new(@field)
+     
+    #多分メインの処理
     loop do
-      
-      break if story02 == true
-      
-      
-      #正直for文を使っているのが信じられん・・・
-      @fieldclone = @field.mat.dup
-      #もしfield.mat[][]がnil以外なら
-      for i in 0...6
-        for j in 0...13
-          unless @field.mat[i][j] == nil
-            #こいつが４以上ならもう一度-1で捜索して-1をnilに置き換える
-            if puyo(i, j, @field.mat) >= 4
-              deletepuyo(i, j, @field.mat)
-            end
-          end
-        end
-      end
-      
-      @field.tdisp
-      @field.mat.map! do |ary|
-        unless ary == []
-          ary = ary.compact
-        else
-          ary = []
-        end
-      end
-      
-      @chainecount += 1
-      p "#{@chainecount}chaine"
-      @field.tdisp
-    end
-  end
-  ###############メソッドども##################
-  #フィールドにランダムの数字を代入
-  def story01
-    p "story01"
-    @field.mat.map! do |ary|
-      ary.map! do |i|
-        i = rand(4) + 1
-      end
+      break if story01 == true
+      story02
+      Tdisp.new(@field)
+      story03
+      story04 #chainカウント+１
+      Tdisp.new(@field)
     end
   end
   
+  attr_accessor :field, :chainecount
+  
+  
+  ###############メソッドども##################  
   #連鎖終了か判断,二つの行列が一致するか判断
-  def story02
+  def story01
     judge_count = 1
     5.times do |i|
-      if @field.mat[i].size == @fieldclone[i].size
+      if @field[i].size == @fieldclone[i].size
         judge_count += 1
       end
     end
     judge_count == 6 ? true : false
   end
   
+  #nilに置き換える
+  def story02
+    @fieldclone = @field.dup
+    #もしfield[][]がnil以外なら
+    for i in 0...6
+      for j in 0...13
+        unless @field[i][j] == nil
+          #こいつが４以上ならもう一度-1で捜索して-1をnilに置き換える
+          if puyo(i, j, @field) >= 4
+            deletepuyo(i, j, @field)
+          end
+        end
+      end
+    end
+  end
+  
+  def story03
+    @field.map! do |ary|
+      unless ary == []
+        ary = ary.compact
+      else
+        ary = []
+      end
+    end
+  end
+  
+  def story04
+    @chainecount += 1
+    p "#{@chainecount}chaine"
+  end
   
   ###########再帰的な方のメソッドども###############
   #なにかのメソッド,探索のみ？必要？
@@ -132,7 +125,12 @@ field.mat.map! do |ary|
     i = rand(4) + 1
   end
 end
-Chain.new(field.mat)
+ok = Chain.new(field.mat)
+
+p "###########"
+p ok.field
+p ok.chainecount
+
 
 
 
